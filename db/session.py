083e5239ddeb425024ddee9f00bd134d00a53c6f
David Cohen <david.cohen@haloinvesting.com>
2019-01-28 09:17:11 -0600

    Rough draft caching class

--- db/session.py
+++ db/session.py
@@ -2,6 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
+# todo: make the database name/user/password configurable:
 dbpath = 'postgresql://david:david@localhost/findb'
 engine = create_engine(dbpath)
 Session = sessionmaker(bind=engine)
